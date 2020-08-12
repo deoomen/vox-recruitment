3946ca4a62ed64f6492e0279678f5040f230b666
deoomen <deoomen@protonmail.com>
2020-08-12 21:26:49 +0200

    some validation

--- resources/js/main.js
+++ resources/js/main.js
@@ -19,7 +19,7 @@ $(() => {
   comments.forEach(comment => {
     const $comment = $commentTemplate.clone();
     $comment.find('.comment__author').text(comment.author);
-    $comment.find('.comment__date').text(comment.date);
+    $comment.find('.comment__date').text(comment.date)  ;
     $comment.find('.comment__text').text(comment.text);
     $('.comments__container').append($comment);
   });
@@ -29,5 +29,39 @@ $(() => {
   });
   $('.counter-text').on('input', (e) => {
     countText(e.target);
+  });
+
+  $('body').on('input', '.is-invalid', (e) => {
+    $(e.target).removeClass('is-invalid').next().children('.help-text').text('');
+  });
+
+  $('#commentForm').on('submit', (e) => {
+    e.preventDefault();
+
+    const $form = $(e.target);
+    let hasError = false;
+
+    $form.find('.has-error,.is-invalid').removeClass('has-error is-invalid');
+    $form.find('.help-text').text('');
+
+    $form.find('.required').toArray().forEach((elem) => {
+      console.log(elem);
+      if ($(elem).val().length === 0) {
+        $(elem).addClass('is-invalid').parents('.form-row').addClass('has-error');
+        $(elem).next().children('.help-text').text('To pole jest wymagane');
+        hasError = true;
+      }
+    });
+
+    if (hasError) {
+      return;
+    }
+
+
+  });
+
+  $('#commentModal').on('hidden.bs.modal', (e) => {
+    $(e.target).find('.has-error,.is-invalid').removeClass('has-error is-invalid');
+    $('#commentForm').trigger('reset');
   });
 });
