c5fa16bff1876a18f8c2099b3e4b6be2a8967007
deoomen <deoomen@protonmail.com>
2020-08-13 21:15:23 +0200

    refactor

--- api/index.php
+++ api/index.php
@@ -6,32 +6,29 @@ use VOXApi\Endpoints\Comments;
 
 // \var_dump($_SERVER, $_GET);
 
-switch ($_SERVER["REQUEST_METHOD"]) {
-    case "GET":
-        break;
-
-    case "POST":
-        break;
-
-    default:
-        header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
-        exit;
+if (!\in_array($_SERVER["REQUEST_METHOD"], ["GET", "POST"])) {
+    \header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
+    exit;
 }
 
 $api = null;
-switch ($_GET["endpoint"]) {
-    case "comments":
+switch ($_SERVER["REQUEST_METHOD"] . $_GET["endpoint"]) {
+    case "GETcomments":
         $api = new Comments();
+        $items = $api->getItems();
+        \var_dump($items);
+        $return = $items;
         break;
 
     default:
-        header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
+        \header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request", true, 400);
         exit;
 }
 
-$items = $api->getItems();
-\var_dump($items);
-$return = $items;
+if (empty($return)) {
+    \header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
+    exit;
+}
 
 // header("Content-Type: application/json;charset=utf-8");
 echo \json_encode($return);
