7f1ae3cd2002b0de7a35027995c4b9780125798a
deoomen <deoomen@protonmail.com>
2020-08-13 20:41:20 +0200

    get items array of objects

--- api/src/endpoints/Comments.php
+++ api/src/endpoints/Comments.php
@@ -3,13 +3,43 @@
 namespace VOXApi\Endpoints;
 
 use VOXApi\Endpoints\Api;
+use VOXApi\Models\Comment;
 
 class Comments extends Api
 {
+    private string $tableName = "comment";
+    private int $page;
+    private int $perPage;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->page = 0;
+        $this->perPage = 10;
+    }
+
     public function getItems(): array
     {
-        return [
-            1,2,3
-        ];
+        $query = "SELECT `c`.`id`, `c`.`author`, `c`.`text`, `c`.`created_at`
+            FROM `{$this->tableName}` AS `c`
+            ORDER BY `c`.`created_at` DESC
+            LIMIT {$this->page}, {$this->perPage}
+        ";
+
+        $stmt = $this->database->connection()->prepare($query);
+        $stmt->bindParam(":page", $this->page);
+        $stmt->bindParam(":perPage", $this->perPage);
+
+        $items = [];
+        if ($stmt->execute()) {
+            while ($row = $stmt->fetch()) {
+                // \var_dump($row);exit;
+                $items[] = new Comment($row);
+            }
+        }
+
+
+        return $items;
     }
 }
