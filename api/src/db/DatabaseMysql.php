5a8ac648f808938db58c9f30207da0d3812b24a1
deoomen <deoomen@protonmail.com>
2020-08-13 20:39:38 +0200

    refactor load config, connection return

--- api/src/db/DatabaseMysql.php
+++ api/src/db/DatabaseMysql.php
@@ -13,27 +13,26 @@ class DatabaseMysql
 
     public function __construct()
     {
-        try {
-            if (!($config = \parse_ini_file(__DIR__ . "/../config/config.ini", true))) {
-                throw new \Exception("Unable to load config file.");
-            }
-
-            $this->host = $config["database"]["host"];
-            $this->port = $config["database"]["port"];
-            $this->username = $config["database"]["username"];
-            $this->password = $config["database"]["password"];
-            $this->dbname = $config["database"]["dbname"];
-        } catch (\Exception $ex) {
-            $this->log($ex->getMessage());
+        if (!($config = \parse_ini_file("config/config.ini", true))) {
+            $message = "Unable to load config file.";
+            $this->log($message);
+            throw new \Exception($message);
         }
+
+        $this->host = $config["database"]["host"];
+        $this->port = $config["database"]["port"];
+        $this->username = $config["database"]["username"];
+        $this->password = $config["database"]["password"];
+        $this->dbname = $config["database"]["dbname"];
+        $this->connection = null;
     }
 
     private function log(string $message): void
     {
-        \error_log(\date("c") . " - " . $message . "\n", 3, __DIR__ . "/../logs/" . \date("Ymd") . "_db_errors.log");
+        \error_log(\date("c") . " - " . $message . "\n", 3, "logs/" . \date("Ymd") . "_db_errors.log");
     }
 
-    public function connect(): bool
+    public function connection(): ?\PDO
     {
         if ($this->connection === null) {
             try {
@@ -42,7 +41,7 @@ class DatabaseMysql
                     $this->username,
                     $this->password,
                     [
-                        \PDO::ATTR_DEFAULT_FECTH_MODE => \PDO::FETCH_OBJ,
+                        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
                         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
                     ]
                 );
@@ -52,10 +51,9 @@ class DatabaseMysql
                     "Line: {$ex->getLine()}",
                     "Message: {$ex->getMessage()}"
                 ]));
-                return false;
             }
         }
 
-        return true;
+        return $this->connection;
     }
 }
