9cb1a9796725d0fe4984df6e89f762256c8512e8
deoomen <deoomen@protonmail.com>
2020-08-13 21:31:26 +0200

    full jquery, mark as template

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ function vendorTask(done) {
     .pipe(gulp.dest(distDir + '/css'));
   gulp
     .src([
-      'node_modules/jquery/dist/jquery.slim.min.js',
+      'node_modules/jquery/dist/jquery.min.js',
       'node_modules/popper.js/dist/umd/popper.min.js',
       'node_modules/bootstrap/dist/js/bootstrap.min.js'
     ])
