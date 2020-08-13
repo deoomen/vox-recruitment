158eb7292d36a813668979178188c59cfa7a514e
deoomen <deoomen@protonmail.com>
2020-08-13 20:40:28 +0200

    database field in Api

--- api/src/endpoints/Api.php
+++ api/src/endpoints/Api.php
@@ -2,7 +2,16 @@
 
 namespace VOXApi\Endpoints;
 
+use VOXApi\Db\DatabaseMysql;
+
 abstract class Api
 {
+    protected DatabaseMysql $database;
+
+    public function __construct()
+    {
+        $this->database = new DatabaseMysql();
+    }
+
     public abstract function getItems(): array;
 }
